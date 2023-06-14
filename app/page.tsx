4e844d62cdb489f59e1bd27ebafac8a3db81f62e
José Daniel Henriques Porto <jdanielhenriques@gmail.com>
2023-06-14 13:49:58 +0100

    NoAuth header page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
 import styles from "@styles/HomeNoAuth.module.scss";
+import HeaderNoAuth from "@components/HomeNoAuth/HeaderNoAuth/page";
 
 const HomeNoAuth = () => {
   return (
     <>
       <main>
-        
+        <HeaderNoAuth />
       </main>
     </>
   );
---
+++ components/HomeNoAuth/HeaderNoAuth/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Button, Container } from "reactstrap";
+import styles from "./styles.module.scss";
+
+import React from "react";
+import Link from "next/link";
+
+const HeaderNoAuth = () => {
+  return (
+    <>
+      <div className={styles.ctaSection}>
+        <img
+          src="homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+        <p>Se cadastre para ter acesso aos cursos</p>
+        <img
+          src="homeNoAuth/logoCta.png"
+          alt="logoCta"
+          className={styles.imgCta}
+        />
+      </div>
+      <Container className={styles.nav}>
+        <img
+          src="/logoOnebitflix.svg"
+          alt="logo Onebitflix"
+          className={styles.imgLogoNav}
+        />
+        <div>
+          <Link href="/login">
+            <Button outline color="primary" className={styles.navBtn}>
+              Entrar
+            </Button>
+          </Link>
+          <Link href="/registro">
+            <Button outline color="primary" className={styles.navBtn}>
+              Quero fazer parte
+            </Button>
+          </Link>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default HeaderNoAuth;
