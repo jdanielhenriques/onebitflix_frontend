bbdedf1b8fe2cd607721eca699dd660307ddb2a3
José Daniel Henriques Porto <jdanielhenriques@gmail.com>
2023-06-15 21:17:02 +0100

    Cards section homeNoAuth - elements, styles and responsivity

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Head from "next/head";
 import styles from "@styles/HomeNoAuth.module.scss";
 import HeaderNoAuth from "@components/HomeNoAuth/HeaderNoAuth/page";
+import CardSection from "@components/HomeNoAuth/CardSection/page";
 import PresentationSection from "@components/HomeNoAuth/presentationSection/page";
 
 const HomeNoAuth = () => {
   return (
     <>
-      <div className={styles.sectionBackground}>
-        <main>
+      <main>
+        <div className={styles.sectionBackground}>
           <HeaderNoAuth />
           <PresentationSection />
-        </main>
-      </div>
+        </div>
+        <CardSection />
+      </main>
     </>
   );
 };
